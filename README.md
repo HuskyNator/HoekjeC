b3c46ab44ab5e4b34c7c9cf68890a530a626d47c
Paul Adriaanse <p.e.a.adriaanse@student.tudelft.nl>
2020-06-12 16:51:36 +0200

    Logo Toevoegen

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-HoekjeC
+# HoekjeC
+
+#### Logo Aanpassen:
+- Een `<iconaam>`.ico aanmaken (bij voorbeeld met paint.net addon).
+- Een `<rcnaam>`.rc bestand de regel `id ICON "<iconaam>.ico"` geven.
+- `windres \<rcnaam>.rc -o \<coffnaam>.coff`
+- In task.json `<coffnaam>.coff` toevoegen als argument onder de te schakelen bestanden (zoals de c bestanden).
--- src/logo.h
+++ src/logo.h
@@ -20,14 +20,14 @@ void maakLogo(/*unsigned int grootte*/)
 {
     logo_verver = maakVerver("./shaders/normaal.vert", "./shaders/normaal.frag");
     const float rode_hoeken[] = {
-        -0.9 / SCHERM_BREEDTE * SCHERM_HOOGTE * 0.15, 1 * 0.015, -0.1,
-        -0.7 / SCHERM_BREEDTE * SCHERM_HOOGTE * 0.15, -2.5 * 0.015, -0.1,
-        1.0 / SCHERM_BREEDTE * SCHERM_HOOGTE * 0.15, 1 * 0.015, -0.1};
+        -0.9 / SCHERM_BREEDTE * SCHERM_HOOGTE * 0.15*2, 1 * 0.015*2, -0.1,
+        -0.7 / SCHERM_BREEDTE * SCHERM_HOOGTE * 0.15*2, -2.5 * 0.015*2, -0.1,
+        1.0 / SCHERM_BREEDTE * SCHERM_HOOGTE * 0.15*2, 1 * 0.015*2, -0.1};
 
     const float blauwe_hoeken[] = {
-        (-1.0 / SCHERM_BREEDTE * SCHERM_HOOGTE + 0.1) * 0.15, (1 + 0.05) * 0.15, 0,
-        (-0.5 / SCHERM_BREEDTE * SCHERM_HOOGTE + 0.1) * 0.15, (-1 + 0.05) * 0.15, 0,
-        (1.0 / SCHERM_BREEDTE * SCHERM_HOOGTE + 0.1) * 0.15, (0.5 + 0.05) * 0.15, 0};
+        (-1.0 / SCHERM_BREEDTE * SCHERM_HOOGTE + 0.1) * 0.15*2, (1 + 0.05) * 0.15*2, 0,
+        (-0.5 / SCHERM_BREEDTE * SCHERM_HOOGTE + 0.1) * 0.15*2, (-1 + 0.05) * 0.15*2, 0,
+        (1.0 / SCHERM_BREEDTE * SCHERM_HOOGTE + 0.1) * 0.15*2, (0.5 + 0.05) * 0.15*2, 0};
 
     const unsigned int hoektallen[] = {
         0, 1, 2};
@@ -47,7 +47,7 @@ void tekenLogo()
     tekenVoorwerp(rood_driehoek);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glLineWidth(2);
+    glLineWidth(4);
     zetVerverFloat4v(logo_verver, "voorwerp_kleur", WIT);
     tekenVoorwerp(rood_driehoek);
     tekenVoorwerp(blauw_driehoek);
